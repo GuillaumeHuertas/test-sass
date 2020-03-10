7d15c3f3cfb344f0c090838f73485de298282d5c
Guillaume Huertas <guillaumehuertas@free.fr>
2020-03-10 21:30:26 +0100

    Minifie css et js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
 
 gulp.task('sass', function() {
     return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
@@ -11,6 +14,8 @@ gulp.task('sass', function() {
 gulp.task('useref', function(){
     return gulp.src('app/*.html')
         .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'))
 });
 
